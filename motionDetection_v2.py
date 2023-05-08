dfc43efe331c34fc048cc6f6dd91a2b1b2e7af94
galleg123 <mikkelkp360@gmail.com>
2023-05-08 12:31:47 +0200

    New code for motion detection which doesn't have to calculate all contours and look through these, this also eliminates the need for dilation and blurring, which results in an easier processing process.
    Now all it does is convert image to greyscale, takes the absolute difference between the image and the previous image, and then thresholds the image to get a matrix of 0's and 1's which it then takes the sum of. This sum then determines whether or not it is considered movement.

---
+++ motionDetection_v2.py
@@ -0,0 +1,95 @@
+import cv2 as cv
+import numpy as np
+import time
+
+
+###### Camera input here #######
+
+
+
+
+
+
+
+################################
+
+
+
+class motion_detector():
+
+    def __init__(self, threshold):
+
+        self.threshold = threshold
+        self.time_last = time.perf_counter()
+        self.detected = False
+        self.detect_timer = 0
+        self.prev_frame = None
+        self.running = True
+        input_video = "test_video.avi"
+
+        self.cam = cv.VideoCapture(input_video)
+
+        
+    def image_grabber(self):
+        return self.cam.read()
+
+    def run(self):
+        while self.running:
+            
+
+            ret, frame = self.image_grabber()
+
+            self.frame = frame
+
+            if not ret:
+                break
+
+
+            # Convert to grayscale
+            processed_frame = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+
+
+            # If this is the first captured frame, save it and do nothing else.
+            if (self.prev_frame is None):
+                self.prev_frame = processed_frame
+                continue
+
+                
+            self.compare_frames(processed_frame)
+
+
+    def compare_frames(self, new_frame):
+            
+
+        # Calculation of the difference between the two frames
+        frame_diff = cv.absdiff(src1 = self.prev_frame, src2 = new_frame)
+        
+
+        # Threshold to only take big enough differences
+        frame_threshold = cv.threshold(src=frame_diff, thresh=self.threshold, maxval=1, type=cv.THRESH_BINARY)[1]
+
+        # Sum of threshold must be above value to be considered motion
+        if np.sum(frame_threshold) > 200: # change this value to change the amount of pixel which have to be changed
+            if self.detected==False:
+                self.detected=True
+                print(f'Detected: {self.detected}')
+        else:
+            if self.detected == True:
+                self.detected = False
+                print(f'Detected: {self.detected}')
+
+
+
+        cv.imshow('Motion detector', self.frame)
+
+        if (cv.waitKey(66) == 27):
+            cv.destroyAllWindows()
+            self.running = False
+
+        self.prev_frame = new_frame
+        
+
+if __name__ == "__main__":
+    motionDetecter = motion_detector(40)
+
+    motionDetecter.run()
