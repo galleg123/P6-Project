77c4203ae025390bc1cf73919b6c5a7a0483ab09
Christian Faurholt <faur@pop-os.localdomain>
2023-06-15 23:20:07 +0200

    For tomorrow

--- Project Test Figures/graphTime.py
+++ Project Test Figures/graphTime.py
@@ -9,8 +9,8 @@ file1 = file1[file1.notnull().sum(axis=1) == 3]  # Filter rows with three values
 file2 = pd.read_csv('P6-Project/Algorithm/time_taking.csv')
 file2 = file2[file2.notnull().sum(axis=1) == 3]  # Filter rows with three values
 
-plt.plot(range(len(file1)), file1.iloc[:, 1], label='No convexity')
-plt.plot(range(len(file2)), file2.iloc[:, 1], label='With convexity')
+plt.plot(range(len(file1)), file1.iloc[:, 1], label='5 Features')
+plt.plot(range(len(file2)), file2.iloc[:, 1], label='7 Features')
 plt.xlabel('Frame', fontsize=20)
 plt.ylabel('Execution time', fontsize=20)
 plt.legend(fontsize=16)
